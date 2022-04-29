ac55c29b77d1e6659ec5144a1315b9e9676bab77
Nam <81206242+nhnam5@users.noreply.github.com>
2022-04-29 21:39:26 +0700

    update title

--- BookMan.ConsoleApp/Program.cs
+++ BookMan.ConsoleApp/Program.cs
@@ -14,6 +14,7 @@ namespace BookMan.ConsoleApp
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Title = "BookMan";
 
             ConfigRouter();
                 
